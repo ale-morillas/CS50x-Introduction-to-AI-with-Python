ef38ff2d1e65825735ba41a8f484d6c7b8ab1c60
ale-morillas <alecorm.edx@gmail.com>
2023-12-31 12:22:34 -0500

    Fix code traffic.py

--- Project 5/1- Traffic/traffic.py
+++ Project 5/1- Traffic/traffic.py
@@ -1,43 +1,39 @@
-Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import cv2
-... import numpy as np
-... import os
-... import sys
-... import tensorflow as tf
-... 
-... from sklearn.model_selection import train_test_split
-... 
-... EPOCHS = 10
-... IMG_WIDTH = 30
-... IMG_HEIGHT = 30
-... NUM_CATEGORIES = 43
-... TEST_SIZE = 0.4
-... 
-... 
-... def main():
-... 
-...     # Check command-line arguments
-...     if len(sys.argv) not in [2, 3]:
-...         sys.exit("Usage: python traffic.py data_directory [model.h5]")
-... 
-...     # Get image arrays and labels for all image files
-...     images, labels = load_data(sys.argv[1])
-... 
-...     # Split data into training and testing sets
-...     labels = tf.keras.utils.to_categorical(labels)
-...     x_train, x_test, y_train, y_test = train_test_split(
-...         np.array(images), np.array(labels), test_size=TEST_SIZE
-...     )
-... 
-...     # Get a compiled neural network
-...     model = get_model()
-... 
-...     # Fit model on training data
-...     model.fit(x_train, y_train, epochs=EPOCHS)
-... 
-...     # Evaluate neural network performance
-...     model.evaluate(x_test,  y_test, verbose=2)
+import cv2
+import numpy as np
+import os
+import sys
+import tensorflow as tf
+
+from sklearn.model_selection import train_test_split
+
+EPOCHS = 10
+IMG_WIDTH = 30
+IMG_HEIGHT = 30
+NUM_CATEGORIES = 43
+TEST_SIZE = 0.4
+
+
+def main():
+    # Check command-line arguments
+    if len(sys.argv) not in [2, 3]:
+        sys.exit("Usage: python traffic.py data_directory [model.h5]")
+
+    # Get image arrays and labels for all image files
+    images, labels = load_data(sys.argv[1])
+
+    # Split data into training and testing sets
+    labels = tf.keras.utils.to_categorical(labels)
+    x_train, x_test, y_train, y_test = train_test_split(
+        np.array(images), np.array(labels), test_size=TEST_SIZE)
+
+    # Get a compiled neural network
+    model = get_model()
+
+    # Fit model on training data
+    model.fit(x_train, y_train, epochs=EPOCHS)
+
+    # Evaluate neural network performance
+    model.evaluate(x_test,  y_test, verbose=2)
 
     # Save model to file
     if len(sys.argv) == 3:
@@ -98,8 +94,7 @@ def get_model():
         tf.keras.layers.Dropout(0.4),
 
         # Output layer with 43 units
-        tf.keras.layers.Dense(NUM_CATEGORIES, activation='softmax')
-    ])
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation='softmax')])
 
     model.compile(
         optimizer='adam',
@@ -110,3 +105,4 @@ def get_model():
     return model
 
 if __name__ == "__main__":
+    main()
