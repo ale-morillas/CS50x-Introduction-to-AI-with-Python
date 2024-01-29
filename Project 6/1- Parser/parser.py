3df8982c0aa9133649a3d417fd383b738f66fb53
Alejandro Corrales Morillas <alecorm.edx@gmail.com>
2024-01-29 14:58:48 -0500

    Update parser.py

--- Project 6/1- Parser/parser.py
+++ Project 6/1- Parser/parser.py
@@ -1,43 +1,40 @@
-Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import nltk
-... import sys
-... 
-... TERMINALS = """
-... Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
-... Adv -> "down" | "here" | "never"
-... Conj -> "and" | "until"
-... Det -> "a" | "an" | "his" | "my" | "the"
-... N -> "armchair" | "companion" | "day" | "door" | "hand" | "he" | "himself"
-... N -> "holmes" | "home" | "i" | "mess" | "paint" | "palm" | "pipe" | "she"
-... N -> "smile" | "thursday" | "walk" | "we" | "word"
-... P -> "at" | "before" | "in" | "of" | "on" | "to"
-... V -> "arrived" | "came" | "chuckled" | "had" | "lit" | "said" | "sat"
-... V -> "smiled" | "tell" | "were"
-... """
-... 
-... NONTERMINALS = """
-... S -> NP VP | NP VP Conj NP VP | NP VP Conj VP
-... 
-... NP -> N | Det N | Det AP N | P NP | NP P NP
-... VP -> V | Adv VP | V Adv | VP NP | V NP Adv
-... PP -> P NP
-... AP -> Adj | AP Adj
-... """
-... 
-... grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
-... parser = nltk.ChartParser(grammar)
-... 
-... 
-... def main():
-... 
-...     # If filename specified, read sentence from file
-...     if len(sys.argv) == 2:
-...         with open(sys.argv[1]) as f:
-...             s = f.read()
-... 
-...     # Otherwise, get sentence as input
-...     else:
+import nltk
+import sys
+
+ TERMINALS = """
+ Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
+ Adv -> "down" | "here" | "never"
+ Conj -> "and" | "until"
+ Det -> "a" | "an" | "his" | "my" | "the"
+ N -> "armchair" | "companion" | "day" | "door" | "hand" | "he" | "himself"
+ N -> "holmes" | "home" | "i" | "mess" | "paint" | "palm" | "pipe" | "she"
+ N -> "smile" | "thursday" | "walk" | "we" | "word"
+ P -> "at" | "before" | "in" | "of" | "on" | "to"
+ V -> "arrived" | "came" | "chuckled" | "had" | "lit" | "said" | "sat"
+ V -> "smiled" | "tell" | "were"
+ """
+ 
+ NONTERMINALS = """
+ S -> NP VP | NP VP Conj NP VP | NP VP Conj VP
+
+ NP -> N | Det N | Det AP N | P NP | NP P NP
+ VP -> V | Adv VP | V Adv | VP NP | V NP Adv
+ PP -> P NP
+ AP -> Adj | AP Adj
+ """
+ 
+ grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
+ parser = nltk.ChartParser(grammar)
+
+ def main():
+ 
+     # If filename specified, read sentence from file
+     if len(sys.argv) == 2:
+        with open(sys.argv[1]) as f:
+             s = f.read()
+ 
+     # Otherwise, get sentence as input
+     else:
         s = input("Sentence: ")
 
     # Convert input into list of words
